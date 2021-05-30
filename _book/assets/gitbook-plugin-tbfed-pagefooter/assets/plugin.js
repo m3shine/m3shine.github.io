dfc34a54677de0703b9f75fd8cbf0b8cb8abc2c9
m3shine <924134616@qq.com>
2021-05-30 13:18:44 +0800

    publish

--- _book/assets/gitbook-plugin-tbfed-pagefooter/assets/plugin.js
+++
@@ -1,9 +0,0 @@
-
-function gitalkshow(){
-  let sty = document.getElementById("gitalk-container").style.display
-  if (sty == "" || sty == "none") {
-    document.getElementById("gitalk-container").style.display = "block"
-  }else{
-    document.getElementById("gitalk-container").style.display = "none !important"
-  }
-}
