02f1e6b70db2af7aef129e6e6faf5c51cdbe14a9
m3shine <924134616@qq.com>
2021-05-30 12:54:51 +0800

    publish

--- docs/gitbook/gitbook-plugin-tbfed-pagefooter/plugin.js
+++ docs/gitbook/gitbook-plugin-tbfed-pagefooter/plugin.js
@@ -1,7 +1,7 @@
 
 function gitalkshow(){
   let sty = document.getElementById("gitalk-container").style.display
-  if (sty == "" || sty == "none") {
+  if (sty !== "block") {
     document.getElementById("gitalk-container").style.display = "block"
   }else{
     document.getElementById("gitalk-container").style.display = "none !important"
